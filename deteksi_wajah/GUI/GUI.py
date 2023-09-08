ef109b0077f888c7fac6a29a49d2c1354ed69d24
aji sasongko <57887588+jisong710@users.noreply.github.com>
2023-09-08 14:55:10 +0700

    sum

--- deteksi_wajah/GUI/GUI.py
+++ deteksi_wajah/GUI/GUI.py
@@ -1,16 +1,43 @@
-# from kivy.app import App
+import cv2
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.clock import Clock
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.graphics.texture import Texture
+
 class GUI(MDApp):
     def build(self):
+        self.title = "Aplikasi OpenCV di Kivy"
+        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.theme_style = "Light"
         return Builder.load_file('tampilan.kv')
+
     def on_start(self):
-        # Set timer untuk berpindah ke halaman utama setelah 3 detik (contohnya)
-        Clock.schedule_once(self.change_to_main, 2)  # Ganti 3 dengan jumlah detik yang Anda inginkan
+        Clock.schedule_once(self.change_to_main, 3)
 
     def change_to_main(self, dt):
-        # Kode untuk berpindah ke halaman utama
         self.root.current = 'main'
+
+class MyBoxLayout(BoxLayout):
+    def __init__(self, **kwargs):
+        super(MyBoxLayout, self).__init__(**kwargs)
+        self.image_widget = Image()
+        self.add_widget(self.image_widget)
+        self.capture = cv2.VideoCapture(0)
+        Clock.schedule_interval(self.update_frame, 1.0 / 30.0)
+
+    def update_frame(self, dt):
+        ret, frame = self.capture.read()
+        frame = cv2.flip(frame, -1)
+        if ret:
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='rgb')
+            texture.blit_buffer(frame_rgb.tobytes(), colorfmt='rgb', bufferfmt='ubyte')
+            self.image_widget.texture = texture
+
+    def on_stop(self):
+        self.capture.release()
+
 if __name__ == '__main__':
     GUI().run()
